dae9406521071385f7921c46aae0c5a67d6c8994
PascalWue <pascal.wuensche@iu-study.org>
2024-06-11 22:07:49 +0200

    Wieder das Original

--- Test/Testcode HTML/Startseite.php
+++ Test/Testcode HTML/Startseite.php
@@ -8,12 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="styles.css">
-
-    Das ist ein Test!!!
-    TEst12sn01010
-
-    
+    <link rel="stylesheet" type="text/css" href="styles.css">    
 </head>
 <body>
 
