c505d63a25fd3cca9d5447f1e38be738a26924a3
sefzikflo <166111934+sefzikflo@users.noreply.github.com>
2024-07-01 14:48:34 +0200

    Verifizierung gegen Hash eingebaut
    #92

--- Aktuell/LoginMethoden.php
+++ Aktuell/LoginMethoden.php
@@ -1,40 +1,45 @@
 <?php
 
-require_once 'Config/config.php';
-
-
-// Verbindung erstellen
-$conn = new mysqli($servername, $username, $password, $dbname);
+require_once 'Backend/config.php';
+$conn1 = new mysqli($servername, $username, $password, $dbname);
 
 // Überprüfen, ob die Verbindung erfolgreich war
-if ($conn->connect_error) {
-    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+if ($conn1->connect_error) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn1->connect_error);
 }
-
-// Login-Daten 91
-$loginBenutzername = $_POST['username'];
-$loginPasswort = $_POST['password'];
-
 
 // Funktion zur Verarbeitung von Login-Daten 92
 function login($loginBenutzername, $loginPasswort) {
-    $stmt = $GLOBALS['conn']->prepare("SELECT * FROM logins WHERE LoginName = ? AND password = ?");
-    $stmt->bind_param("ss", $loginBenutzername, $loginPasswort);
+    $stmt = $GLOBALS['conn1']->prepare("SELECT * FROM logins WHERE LoginName = ?");
+    $stmt->bind_param("s", $loginBenutzername);
     $stmt->execute();
     $result = $stmt->get_result();
 
+    $row = $result->fetch_assoc();
+    $hashedPW = $row['password'];
+
+
     if ($result->num_rows > 0) {
-        return true; // Login erfolgreich
-    } else {
+       if(password_verify($loginPasswort, $hashedPW)){
+            return true; // Login erfolgreich
+        }
+        else {
+            return false;
+        }
+    } 
+    else {
         return false; // Login fehlgeschlagen
     }
 }
 
-if (login($loginBenutzername, $loginPasswort)){
-    header("Location: Test/Testcode HTML/Startseite.php");
-}
-else{
-    header("Location: Test/Testcode HTML/LoginScreen.php?success=false&login=".$loginBenutzername);
+function mitarbeiterAnlegen($name, $password){
+
+    $hashedPW = password_hash($password, PASSWORD_BCRYPT);
+
+    $stmt = $GLOBALS['conn1']->prepare("INSERT INTO logins (LoginName, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $name, $hashedPW);
+    $stmt->execute();
+    $stmt->close();
 }
 
 ?>
