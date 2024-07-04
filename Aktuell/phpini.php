13b66eb9a56362921152959862c9f50f765b952c
sefzikflo <166111934+sefzikflo@users.noreply.github.com>
2024-07-04 10:01:14 +0200

    Quelle angegeben

--- Aktuell/phpini.php
+++ Aktuell/phpini.php
@@ -1,12 +1,15 @@
 <?php
 
-ini_set('session.gc_maxlifetime', 300);
+// Quelle: https://dev.to/omacys/ensuring-secure-user-sessions-a-guide-to-logging-out-users-due-to-inactivity-in-php-3167
+
+
+ini_set('session.gc_maxlifetime', 1800);
 
 // Start session
 session_start();
 
 if (!isset($_SESSION["Angemeldet"])){
-    header("Location: LoginScreen.php");
+    header("Location: http://restaurantreservierung.42web.io/Test/Testcode%20HTML/LoginScreen.php");
 }
 
 elseif (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > ini_get('session.gc_maxlifetime'))) {
